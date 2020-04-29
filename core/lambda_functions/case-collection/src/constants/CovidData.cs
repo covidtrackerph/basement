c769caefa95333b2ca6deceb55eeeacdc8c52c5e
judedaryl <clarinojd@gmail.com>
2020-04-29 14:41:21 +0800

    Update root folder

--- core/lambda_functions/case-collection/src/constants/CovidData.cs
+++ core/lambda_functions/case-collection/src/constants/CovidData.cs
@@ -3,6 +3,6 @@ namespace CaseCollection
     public static class CovidData
     {
         public const string GoogleDriveApi = "https://www.googleapis.com";
-        public const string RootFolderId = "10VkiUA8x7TS2jkibhSZK1gmWxFM-EoZP";
+        public const string RootFolderId = "1w_O-vweBFbqCgzgmCpux2F0HVB4P6ni2";
     }
 }
