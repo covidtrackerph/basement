47cd67e1b85c5f6f19dd68e939b433d1ff8fc981
Daryl <clarinojd@gmail.com>
2020-05-05 23:10:55 +0800

    Update cron every 6-12

--- core/cloudwatch.tf
+++ core/cloudwatch.tf
@@ -1,8 +1,7 @@
 resource "aws_cloudwatch_event_rule" "covid_drop_collection" {
   name                = "covid-data-drop-collection-${var.namespace}"
   description         = "Managed by Terraform. Collects data from Google Drive between 6pm - 10pm"
-  schedule_expression = "cron(0/3 * 1/1 * ? *)"
-  # "cron(0 18-22 * * ? *)"
+  schedule_expression = "cron(0 18-24 * * ? *)"
 }
 
 resource "aws_cloudwatch_event_target" "covid_collection_target" {
