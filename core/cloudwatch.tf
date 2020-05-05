59775cbd800b4e21cb91da95a33f464fe5ca30d6
Daryl <clarinojd@gmail.com>
2020-05-05 21:49:32 +0800

    fmt

--- core/cloudwatch.tf
+++ core/cloudwatch.tf
@@ -1,13 +1,13 @@
 resource "aws_cloudwatch_event_rule" "covid_drop_collection" {
-    name = "covid-data-drop-collection-${var.namespace}"
-    description = "Managed by Terraform. Collects data from Google Drive between 6pm - 10pm"
-    schedule_expression = "cron(0 0/3 * 1/1 * ? *)"
-    # "cron(0 18-22 * * ? *)"
+  name                = "covid-data-drop-collection-${var.namespace}"
+  description         = "Managed by Terraform. Collects data from Google Drive between 6pm - 10pm"
+  schedule_expression = "cron(0 0/3 * 1/1 * ? *)"
+  # "cron(0 18-22 * * ? *)"
 }
 
 resource "aws_cloudwatch_event_target" "covid_collection_target" {
-    arn = aws_lambda_function.case_collection.arn
-    rule = aws_cloudwatch_event_rule.covid_drop_collection
-    target_id = "covid-data-collection"
-    input = "{\"test\":\"collect\"}"
+  arn       = aws_lambda_function.case_collection.arn
+  rule      = aws_cloudwatch_event_rule.covid_drop_collection
+  target_id = "covid-data-collection"
+  input     = "{\"test\":\"collect\"}"
 }
