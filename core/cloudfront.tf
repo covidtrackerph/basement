00888e3ec2710ac53689f58379e60b8ec6c8ff7b
Daryl <clarinojd@gmail.com>
2020-05-14 05:26:42 +0800

    Test change cache behavior

--- core/cloudfront.tf
+++ core/cloudfront.tf
@@ -88,8 +88,9 @@ resource "aws_cloudfront_origin_access_identity" "covid_tracker_ui" {
 
 resource "aws_cloudfront_distribution" "s3_distribution" {
   origin {
-    domain_name = aws_s3_bucket.covid_tracker_ui.bucket_domain_name
     origin_id   = "S3-${aws_s3_bucket.covid_tracker_ui.bucket}"
+    domain_name = aws_s3_bucket.covid_tracker_ui.bucket_domain_name
+
     s3_origin_config {
       origin_access_identity = aws_cloudfront_origin_access_identity.covid_tracker_ui.cloudfront_access_identity_path
     }
@@ -122,9 +123,15 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   }
 
   default_cache_behavior {
-    allowed_methods  = ["GET", "HEAD"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = "S3-${aws_s3_bucket.covid_tracker_ui.bucket}"
+    target_origin_id       = "S3-${aws_s3_bucket.covid_tracker_ui.bucket}"
+    viewer_protocol_policy = "redirect-to-https"
+    allowed_methods        = ["HEAD", "GET"]
+    cached_methods         = ["HEAD", "GET"]
+    compress               = true
+
+    min_ttl     = 0
+    default_ttl = 86400
+    max_ttl     = 31536000
 
     forwarded_values {
       query_string = false
@@ -133,12 +140,6 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
         forward = "none"
       }
     }
-
-    viewer_protocol_policy = "redirect-to-https"
-    min_ttl                = 0
-    default_ttl            = 0
-    max_ttl                = 0
-
 
     lambda_function_association {
       event_type   = "origin-request"
