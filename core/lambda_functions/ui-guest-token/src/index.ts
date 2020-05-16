44c677f2622210aa58ec7544ce74cb21d997fd48
Daryl <clarinojd@gmail.com>
2020-05-17 06:57:06 +0800

    Modify lambda

--- core/lambda_functions/ui-guest-token/src/index.ts
+++ core/lambda_functions/ui-guest-token/src/index.ts
@@ -1,19 +1,19 @@
 const handler: CloudFrontResponseHandler = (event, _, callback) => {
-    // let { response } = event.Records[0].cf;
-    // let { body } = response;
+    let { response } = event.Records[0].cf;
+    let { body } = response;
 
-    // if (body && body.includes('<title>COVID Tracker Philippines</title>')) {
-    //     response.headers = {
-    //         ...response.headers,
-    //         was: [{key: 'wa', value: 'sap'}]   
-    //     }
-    //     const headEnd = body.split(' </head>');
-    //     body = [headEnd[0], '\t\twasap broski\n\t</head>', headEnd[1]].join('\n')
-    //     response.body = body;
-    // }
-    // return response;
+    if (body && body.includes('<title>COVID Tracker Philippines</title>')) {
+        response.headers = {
+            ...response.headers,
+            was: [{key: 'wa', value: 'sap'}]   
+        }
+        const headEnd = body.split(' </head>');
+        body = [headEnd[0], '\t\twasap broski\n\t</head>', headEnd[1]].join('\n')
+        response.body = body;
+    }
+    return response;
     //Get contents of response
-    const response = event.Records[0].cf.response;
+    // const response = event.Records[0].cf.response;
     const headers = response.headers;
     
     console.log(JSON.stringify(headers));
@@ -21,7 +21,7 @@ const handler: CloudFrontResponseHandler = (event, _, callback) => {
 
     //Set new headers 
     headers['strict-transport-security'] = [{ key: 'Strict-Transport-Security', value: 'max-age=63072000; includeSubdomains; preload' }];
-    headers['content-security-policy'] = [{ key: 'Content-Security-Policy', value: "default-src 'none'; img-src 'self'; script-src 'self'; style-src 'self'; object-src 'none'" }];
+    // headers['content-security-policy'] = [{ key: 'Content-Security-Policy', value: "default-src 'none'; img-src 'self'; script-src 'self'; style-src 'self'; object-src 'none'" }];
     headers['x-content-type-options'] = [{ key: 'X-Content-Type-Options', value: 'nosniff' }];
     headers['x-frame-options'] = [{ key: 'X-Frame-Options', value: 'DENY' }];
     headers['x-xss-protection'] = [{ key: 'X-XSS-Protection', value: '1; mode=block' }];
